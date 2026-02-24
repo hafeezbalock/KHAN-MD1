1817c6472a432ab98dae56ec9a97eb7d7ac7745c
hafeezbalock <63083724+hafeezbalock@users.noreply.github.com>
2026-02-25 04:51:05 +0500

    Update SESSION_ID to use an empty string default

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     // ===== BOT CORE SETTINGS =====
-    SESSION_ID: process.env.SESSION_ID || "https://mega.nz/file/ydAU2QJC#02zmj9cnpun4PmmWWa1yckNXFELxpe41RTDGr6JOVjc",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || "",  // Command prefix (e.g., "., / ! * - +")
     CHATBOT: getConfig("CHATBOT") || "on", // on/off chat bot 
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "KHAN-MD",  // Bot's display name
